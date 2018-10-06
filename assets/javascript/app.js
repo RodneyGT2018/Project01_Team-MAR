8c926433cc091a738a04c36e32de6667ddc36bae
TonyRome <tonyromebx@gmail.com>
2018-10-06 13:42:27 -0400

    wip 10/7

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -73,25 +73,30 @@ var map;
     $('.assigned-block').append(newDiv)
   })
 
+   $("#reset").click(function (e) {
+            location.reload();
+        });
 
-  function JavaScriptFetch() {
-    var script = document.createElement('script');
-    script.src = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + document.getElementById("search").value;;
-    document.querySelector('head').appendChild(script);
-  }
-
-  function jsonFlickrFeed(data) {
-    var image = "";
-    data.items.forEach(function (element) {
-        image += "<img src=\"" + element.media.m + "\"/>";
-    });
-
-    document.getElementById("outputDiv").innerHTML = image;
-  }
-   
   $("#submit").click(function (e) {
       $("#outputDiv").html("");
-
+      var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#search").val();
+      $.ajax({
+          url: flickerAPI,
+          dataType: "jsonp", // jsonp
+          jsonpCallback: 'jsonFlickrFeed', // add this property
+          success: function (result, status, xhr) {
+              $.each(result.items, function (i, item) {
+                  $("<img>").attr("src", item.media.m).appendTo("#outputDiv");
+                  if (i === 10) {
+                      return false;
+                  }
+              });
+          },
+          error: function (xhr, status, error) {
+              console.log(xhr)
+              $("#outputDiv").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+          }
+      });
+      
     });
-
 })
